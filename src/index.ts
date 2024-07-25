1407fd298fe75a2d2e9ef7928c78f0a2a31d79de
DaDavinci <danny09vand@gmail.com>
2024-07-25 17:45:27 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,140 @@
+import WebSocket, { WebSocketServer, createWebSocketStream } from "ws";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 6214;
+
+const connections = {};
+
+function isValidWebSocketURL(url) {
+    return url.startsWith("wss://");
+}
+
+function generateUUID() {
+    return Math.random().toString(36).substring(2, 10);
+}
+
+function handleWebSocketConnection(UUID, socket) {
+    socket.on('message', (message) => {
+        if (connections[UUID]) {
+            const messageString = message.toString();
+            connections[UUID].messages.push({
+                id: generateUUID(),
+                message: messageString,
+                step: connections[UUID].messages.length + 1
+            });
+        }
+    });
+    socket.on('error', (error) => {
+        console.error(`WebSocket error for UUID: ${UUID}`, error);
+        if (connections[UUID]) {
+            connections[UUID].errors.push({
+                id: generateUUID(),
+                message: error,
+                step: connections[UUID].errors.length + 1
+            });
+        }
+    });
+}
+
+
+app.post('/connect', async (req, res) => {
+    const { Socket } = req.body;
+    if (!Socket) {
+        return res.status(400).json({ success: false, error: "No WebSocket URL provided!" });
+    }
+    if (!isValidWebSocketURL(Socket)) {
+        return res.status(400).json({ success: false, error: "Invalid WebSocket URL" });
+    }
+
+    const UUID = generateUUID();
+    const socket = new WebSocket(Socket);
+
+    try {
+        await new Promise((resolve, reject) => {
+            socket.on('error', (error) => {
+                console.error(`WebSocket error for UUID: ${UUID}`, error);
+                reject(error);
+            });
+            socket.on('open', () => {
+                resolve({});
+            });
+        });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: "WebSocket connection error" });
+    }
+
+    connections[UUID] = { socket: socket, messages: [] };
+    handleWebSocketConnection(UUID, socket);
+
+    res.json({ UUID, Socket, success: true });
+    return;
+});
+
+
+app.post('/disconnect', (req, res) => {
+    const { UUID } = req.body;
+    if (!UUID) {
+        return res.status(400).json({ success: false, error: "No UUID provided!" });
+    }
+    if (!connections[UUID]) {
+        return res.status(404).json({ success: false, error: "UUID not found" });
+    }
+
+    connections[UUID].socket.close();
+    delete connections[UUID];
+
+    res.json({ UUID, success: true });
+    return
+});
+
+app.post('/send', (req, res) => {
+    const { UUID, Message } = req.body;
+    if (!UUID || !Message) {
+        return res.status(400).json({ success: false, error: "UUID or Message not provided!" });
+    }
+    if (!connections[UUID] || connections[UUID].socket.readyState !== WebSocket.OPEN) {
+        return res.status(404).json({ success: false, error: "Invalid UUID or WebSocket connection closed" });
+    }
+
+    connections[UUID].socket.send(Message);
+    res.json(true);
+    return
+});
+
+app.post('/get', (req, res) => {
+    const { UUID } = req.body;
+    if (!UUID) {
+        return res.status(400).json({ success: false, error: "No UUID provided!" });
+    }
+    if (!connections[UUID]) {
+        return res.status(404).json({ success: false, error: "Invalid UUID" });
+    }
+
+    res.json(connections[UUID].messages);
+    return
+});
+app.post('/errors', (req, res) => {
+    const { UUID } = req.body;
+    if (!UUID) {
+        return res.status(400).json({ success: false, error: "No UUID provided!" });
+    }
+    if (!connections[UUID]) {
+        return res.status(404).json({ success: false, error: "Invalid UUID" });
+    }
+
+    res.json(connections[UUID].errors);
+    return
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+export default app;
